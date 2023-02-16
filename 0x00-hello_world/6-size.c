bc499ba9f2aa20159575e2e04a42c7fba45122d1
Chidinma <chifineg@yahoo.com>
2023-02-16 12:31:49 -0800

    task 5-printf and 6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,11 @@
  */
 int main(void)
 {
-	int integerType;
-	float floatType;
-	double doubleType;
-	char charType;
+	printf("size of char %lu bytes", sizeof(char));
+	printf("size of int %lu bytes", sizeof(int));
+	printf("size of double %lu bytes", sizeof(double));
+	printf("size of float %lu bytes", sizeof(float));
+	printf("size of long in %lu bytes", sizeof(long int));
+	printf("size of long long int %lu bytes", sizeof(long long int));
 	return (0);
 }
