399d41a7965242edc57175e31875305c3560c4c8
Chidinma <chifineg@yahoo.com>
2023-02-16 11:43:15 -0800

    4-puts.c 7

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-int main(void) /*the man function to print a string to the stout starts here*/
+int main(void) /*
+		  *the man function to print a string to the stout starts here
+		*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 return (0);
