ec3b65dca4153fe357366bc53e7cb9e58ae0e3c6
Chidinma <chifineg@yahoo.com>
2023-02-16 12:02:15 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - function starts here
+ *
+ * Return: when 0 (no error)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\,");
+	return (0);
+}
