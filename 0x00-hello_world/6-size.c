efdc62255c3d69398f153bdf899701ea14debe91
Chidinma <chifineg@yahoo.com>
2023-02-16 12:17:14 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main function starts here
+ *
+ * Return: when 0 (no error)
+ */
+int main(void)
+{
+	int integerType;
+	float floatType;
+	double doubleType;
+	char charType;
+	return (0);
+}
