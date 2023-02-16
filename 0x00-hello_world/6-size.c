6f0093ddecaa2a2172f25105cd9f0e8fc48c6aea
Chidinma <chifineg@yahoo.com>
2023-02-16 12:36:12 -0800

    6-size.c updated code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,10 @@
  */
 int main(void)
 {
-	printf("size of char %lu bytes", sizeof(char));
-	printf("size of int %lu bytes", sizeof(int));
-	printf("size of double %lu bytes", sizeof(double));
-	printf("size of float %lu bytes", sizeof(float));
-	printf("size of long in %lu bytes", sizeof(long int));
-	printf("size of long long int %lu bytes", sizeof(long long int));
+	printf("size of char %lu byte(s)\n", sizeof(char));
+	printf("size of int %lu byte(s)\n", sizeof(int));
+	printf("size of float %lu byte(s)\n", sizeof(float));
+	printf("size of long in %lu byte(s)\n", sizeof(long int));
+	printf("size of long long int %lu byte(s)\n", sizeof(long long int));
 	return (0);
 }
