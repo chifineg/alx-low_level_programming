586ceb855431ac8dbe41214b36ce6d3901ca7f29
Chidinma <chifineg@yahoo.com>
2023-02-17 02:59:31 -0800

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* imported the libraries I need for this program */
+
+/**
+* main - program funtion starts here
+*
+* Return: 0 (when no error)
+*/
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* start of if/else */
+if (n > 0)
+{
+printf("%d is positive", n);
+}
+else if (n == 0)
+{
+printf("%d is zero", n);
+}
+else
+printf("%d is negative", n);
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,3 @@
+We are diving deeper into C programming.
+In this project, we are working on conditional statements. 
+We started with if/else statement.
