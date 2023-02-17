2c737970168422b937a41ab69fb990fb45f90127
Chidinma <chifineg@yahoo.com>
2023-02-17 08:21:28 -0800

    added 6-print_numberz.c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* imported the librabries I need to run the program */
+
+/**
+* main - program function starts here
+*
+* Return: 0 (when no error)
+*/
+int main(void)
+{
+int a = 0;
+for (; a < 10; a++)
+{
+putchar(a);
+}
+return (0);
+}
