12d7020eae375e1ac978163513772753dd1d8f23
Chidinma <chifineg@yahoo.com>
2023-02-17 09:38:50 -0800

    added 101-print_comb4.c file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* imported the librabries I need to run the program */
+
+/**
+* main - program function starts here
+*
+* Return: 0 (when no error)
+*/
+int main(void)
+{
+int i, j, k;
+
+for (i = 0; i < 10; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+for (k = j + 1; k < 10; k++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+if (i != 7 || j != 8 || k != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
