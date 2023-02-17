14a7b560268839b382e019d23231dc7eebefbfba
Chidinma <chifineg@yahoo.com>
2023-02-17 04:38:20 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* imported the librabries I need to run the program */
+
+/**
+* main - program function starts here
+*
+* Return: 0 (when no error)
+*/
+int main(void)
+{
+char alpha;
+alpha = 'a';
+char zaph;
+zaph = 'z';
+while (alpha <= zaph)
+{
+putchar(alpha);
+alpha++;
+}
+putchar('\n');
+return (0);
+}
