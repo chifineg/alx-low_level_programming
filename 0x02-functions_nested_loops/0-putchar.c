e0348e0e6be45f62a12f55938f7fd2c68b70b44b
Chidinma <chifineg@yahoo.com>
2023-02-21 10:17:04 -0800

    added README.md and 0-putchar files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* imported the librabries I need to run the program */
+
+/**
+* main - program function starts here
+*
+* Return: 0 (when no error)
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+On this project, we will be writing programs using loops.
+This tasks will teach us how to use loops in repeating several tasks at a time.
