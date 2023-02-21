e7bb45b8d68f2f474b336960e7d6de22926431e0
Chidinma <chifineg@yahoo.com>
2023-02-21 12:02:12 -0800

    added 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_sign - checks for lowercase character.
+* @n: The characters to be checked.
+* Return: 1 if n is an positive, 0  if zero, -1 is less than zero.
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	return (1);
+	_putchar('+');
+	else if (n == 0)
+	return (0);
+	_putchar(0 + '0');
+	else
+	return (-1);
+	putchar('-');
+}
