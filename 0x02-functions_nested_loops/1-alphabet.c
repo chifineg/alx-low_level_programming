1c691c0406c300d2c423cc8ea38060558205057c
Chidinma <chifineg@yahoo.com>
2023-02-21 11:07:26 -0800

    modified 1-alphabet.c file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,15 @@
 /* imported the main.h library I need to run the program */
 
 /**
-* main - program function starts here
+* print_alphabet - program function starts here
 *
 * Return: 0 (when no error)
 */
 void print_alphabet(void)
 {
 	char c;
-	for(c = 'a'; c <= 'z'; c++)
+
+	for (c = 'a'; c <= 'z'; c++)
 	_putchar(c);
 	_putchar('\n');
 }
