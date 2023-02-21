d2381892fe029180f798cb28d0ce65cf7995c993
Chidinma <chifineg@yahoo.com>
2023-02-21 12:25:44 -0800

    added 6-abs.c file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _abs - compute the absolute value of an integer
+* @n: number to check
+* Return: absolute value of n
+*/
+int _abs(int n)
+{
+if (n < 0)
+{
+n = n * -1;
+}
+return (n);
+}
