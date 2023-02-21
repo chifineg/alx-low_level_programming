4abf0a36e3ef21d7030c861fb99e74370a229dde
Chidinma <chifineg@yahoo.com>
2023-02-21 13:49:00 -0800

    modified 104-fibonnaci.c file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,21 +9,37 @@ followed by a new line
 */
 int main(void)
 {
-int i;
-long int a, b, c;
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1;
+	unsigned long int aft2;
 
-a = 1;
-b = 2;
-printf("%ld, %ld, ", a, b);
-for (i = 0; i < 96; i++)
-{
-c = a + b;
-a = b;
-b = c;
-printf("%ld", c);
-if (i < 95)
-printf(", ");
-}
-printf("\n");
-return (0);
+	printf("%lu", bef);
+
+	for (i = 1; i < 91; i++)
+	{
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
+	}
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
+	}
+	printf("\n");
+	return (0);
 }
