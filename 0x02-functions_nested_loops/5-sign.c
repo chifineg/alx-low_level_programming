5d44930709de59fe68ef72571c69ae00dbebeb87
Chidinma <chifineg@yahoo.com>
2023-02-21 12:04:48 -0800

    modified 5-sign.c file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,12 +8,16 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 	return (1);
 	_putchar('+');
+	}
 	else if (n == 0)
+	{
 	return (0);
 	_putchar(0 + '0');
+	}
 	else
 	return (-1);
-	putchar('-');
+	_putchar('-');
 }
