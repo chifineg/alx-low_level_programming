9b915e49e07f85a6e1e04e24ab21e40dac1f47ce
Chidinma <chifineg@yahoo.com>
2023-02-21 14:57:53 -0800

    modified 1-main.c file

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,14 +10,14 @@ int main(void)
 int i;
 
 printf("Infinite loop incoming :(\n");
-
-/*i = 0;
-
-while (i < 10)
-{
-putchar(i);
-}*/
-
+/**
+*comment - off the block of code causing infinite loop. i = 0;
+*\o/: used to escape character
+*while (i < 10)
+*{
+*putchar(i);
+*}
+*/
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
