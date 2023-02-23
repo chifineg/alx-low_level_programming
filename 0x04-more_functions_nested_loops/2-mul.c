43ce41f38779cf6b4a6e044686177561785d1c2c
Chidinma <chifineg@yahoo.com>
2023-02-23 11:10:43 -0800

    added 2-mul.c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/* imported the library I need to run the program */
+
+/**
+* main - program function starts here
+*@a: variable to multiply
+*@b: variable to multiply
+* Return: 0 (when no error)
+*/
+
+int mul(int a, int b)
+{
+	int sum = a * b;
+	return (sum);
+}
