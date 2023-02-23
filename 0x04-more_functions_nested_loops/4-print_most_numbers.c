7025c466c784071e5d717bb5f9dcf14a268ff71d
Chidinma <chifineg@yahoo.com>
2023-02-23 11:36:09 -0800

    added 4-print_most_numbers.c file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/* imported the library I need to run the program */
+
+/**
+* print_most_numbers - program function starts here
+*
+* Return: 0 (when no error)
+*/
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		if (x != 2 && x != 4)
+			_putchar(x + '0');
+	}
+	_putchar('\n');
+}
