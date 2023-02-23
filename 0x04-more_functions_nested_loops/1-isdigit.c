3c222f2dd6f8ed28697d3117756af727ae708d74
Chidinma <chifineg@yahoo.com>
2023-02-23 11:05:58 -0800

    modified 1-isdigit.c file

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,11 @@
 #include "main.h"
-/* imported the libraryI need to run the program */
 
 /**
-* _isdigit - program function starts here
-*@c: variable to test
-* Return: 1 when digit, 0 when otherwise
-*/
-
-int _isdigit(int c);
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: int to be checked
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-	else
-		return (0);
+	return (c >= '0' && c <= '9');
 }
