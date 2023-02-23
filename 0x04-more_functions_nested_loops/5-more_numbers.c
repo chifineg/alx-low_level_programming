85cfb30a90483c4fab3c2973d42964ce48e0d983
Chidinma <chifineg@yahoo.com>
2023-02-23 11:45:14 -0800

    added 5-more_numbers.c file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/* imported the library I need to run the program */
+
+/**
+* more_numbers - program function starts here
+*
+* Return: 0 (when no error)
+*/
+
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 1; x <= 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			_putchar(y + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
