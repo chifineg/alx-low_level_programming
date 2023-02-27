a87d55b073e3c8f6d6ed319c668945c8fab40a33
Chidinma <chifineg@yahoo.com>
2023-02-27 11:10:41 -0800

    added 1-swap.c file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/* imported the librabries I need to run the program */
+
+/**
+*swap_int - program function starts here
+*@a: number to test
+*@b: number to test
+*/
+
+void swap_int(int *a, int *b)
+{
+	int x;
+	
+	x = *a;
+	*a = *b;
+	*b = x;
+}
