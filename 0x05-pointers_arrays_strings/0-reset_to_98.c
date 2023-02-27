55e3e679cc159cf1257c05ee8610afc6c827ea9b
Chidinma <chifineg@yahoo.com>
2023-02-27 10:50:41 -0800

    added the files I need to start my pointers project, 0-reset_to_98.c file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/* imported the librabries I need to run the program */
+
+/**
+* reset_to_98 - program function starts here
+*@n: number to test
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
