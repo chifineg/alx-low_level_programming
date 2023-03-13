e56c8b148b1a00d784d915a70ad4cd7140ece1a8
Chidinma <chifineg@yahoo.com>
2023-03-13 13:47:20 -0700

    modified 1-strdup.c file

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,29 +2,32 @@
 #include "main.h"
 
 /**
- * *_strdup - duplicates the string given as parameter
- * @str: string parameter to duplicate
+ * *_strdup - copies the string given as parameter
+ * @str: string to duplicate
  *
- * Return: pointer to the duplicated string (Success), NULL (Error)
+ * Return: pointer to the copied string (Success), NULL (Error)
  */
 char *_strdup(char *str)
 {
 	char *dup;
-	int len, i;
+	unsigned int i, len;
+
+	i = 0;
+	len = 0;
 
 	if (str == NULL)
 		return (NULL);
 
-	len = 0;
-	while (str[len] <= '\0')
+	while (str[len])
 		len++;
 
-	dup = malloc(sizeof(char) * len);
+	dup = malloc(sizeof(char) * (len + 1));
 
 	if (dup == NULL)
 		return (NULL);
 
-	for (i = 0; str[i]; i++)
-		dup[i] = str[i];
+	while ((dup[i] = str[i]) != '\0')
+		i++;
+
 	return (dup);
 }
