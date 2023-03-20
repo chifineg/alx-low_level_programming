7bf48783ebf52deb1174081db450f4b62dc971e9
Chidinma <chifineg@yahoo.com>
2023-03-20 12:47:51 -0700

    added 2-main.c file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - program funtion starts here
+*
+* Return: 0 if no error, 1 if error
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
