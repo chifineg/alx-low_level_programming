e4fe04246d3a7073cb2af7ddfd9179e00cc9b37a
Chidinma <chifineg@yahoo.com>
2023-04-06 15:09:11 -0700

    added betty style comment to 0-binary_to_uint.c file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ *binary_to_uint - function to convert from base 2 to base 10
+ *@b: base 2 number to convert
+ *
+ *Return: 0 if NULL and base 10 number if binary
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int unit = 0;
