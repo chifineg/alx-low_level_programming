98330d666b89b50b1ee5eedfe289c9c7f198137e
Chidinma <chifineg@yahoo.com>
2023-04-07 10:28:49 -0700

    modified 0-binary_to_uint.c file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -38,3 +38,22 @@ unsigned int binary_to_uint(const char *b)
 	}
 	return (unit);
 }
+
+/**
+ * power - function to calculate power
+ * @num: number to compute
+ * @raisedTo: exponent
+ *
+ * Return: power of num
+ */
+double power(double num, int raisedTo)
+{
+	double result = 1.0;
+	int i;
+
+	for (i = 0; i < raisedTo; i++)
+	{
+		result *= num;
+	}
+	return (result);
+}
